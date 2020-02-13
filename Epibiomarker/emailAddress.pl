24ba88241550451003b752c1ae018e19e00a4867
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-02-13 17:13:54 -0600

    Add files via upload

--- Epibiomarker/emailAddress.pl
+++ Epibiomarker/emailAddress.pl
@@ -1,11 +1,9 @@
 #!/usr/bin/perl -w
 use strict;
-my $SearchResultFile = shift @ARGV;
+my $SearchResultFile = 'methylation-biomarker.txt';
 open FH, $SearchResultFile or die "Cannot open the $SearchResultFile:$!\n";
-open OUT, ">$SearchResultFile.Author_Address.txt" or die "Cannot create the output file:$!\n";
-print OUT "Email\tTitle\tFirst Name\tMiddle Name\tLast Name\tContribution Title\r\n";
+open OUT, ">result.xls" or die "Cannot create the output file:$!\n";
 my %AuthorInfo;
-my %email;
 my ($tmpLastName, $tmpForeName, $tmpEmail);
 while (<FH>) {
   if (m{LastName>(\w.*)</LastName>}){
@@ -19,30 +17,8 @@ while (<FH>) {
       $tmpEmail = "None";
     }
     if ($tmpEmail =~ m/@/){
-	  next if $tmpEmail=~/@\w.*\s*\w.*@/;
-	  next if $tmpEmail=~/,/;
-	  next if $tmpEmail=~/\>/;
-	  next if $tmpEmail=~/Affiliatio/;
-	  next if $tmpEmail=~/\s+/;
-	  next if $tmpEmail=~/mail\:/;
-	  next if $tmpEmail=~/\:/;
-	  next if $tmpEmail=~/\(/;
-	  next if $tmpEmail=~/\)/;
-	  next if $tmpEmail=~/\//;
-	  next if $tmpEmail=~/\\/;
-	  next if $tmpEmail=~/gov1/;
-	  next if $tmpEmail=~/stanfordedu/;
-      $tmpEmail=lc $tmpEmail;
-	  my @email=split/\./,$tmpEmail;
-	  next if defined $email{$tmpEmail};
-      print OUT "$tmpEmail\tDr\t$tmpForeName\t$tmpLastName\t.\t$tmpLastName\n" if ! defined $email{$tmpEmail};
-	  $email{$tmpEmail}++;
+      print OUT "$tmpLastName\t$tmpForeName\t$tmpEmail\r\n";
       $tmpEmail = "None";
-	  my $country=$email[$#email];
     }
   }
-}
-
-foreach my $emailtemp(sort keys %email){
-print "$emailtemp\t$email{$emailtemp}\n";
 }
